47a2f23d9f890e1b2128222ec3a4a5dd3bb92876
Madhuri <2500040194@kluniversity.in>
2026-03-13 11:54:52 +0530

    modified Home.jsx

--- src/exam/Home.jsx
+++ src/exam/Home.jsx
@@ -5,6 +5,7 @@ function Home() {
     <div>
       <h1>Welcome to Course Portal</h1>
       <p>This is the Home Page.</p>
+      <img src="https://img.freepik.com/free-vector/hand-drawn-online-college-template-design_23-2150574159.jpg?semt=ais_hybrid&w=740&q=80"></img>
     </div>
   );
 }
